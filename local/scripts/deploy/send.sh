8c25c9d88216b22d2400f73f4e28b6bf2c486217
Erick Araujo <batistasd678@gmail.com>
2023-04-10 14:44:16 -0300

    Ajuste nos scripts

--- local/scripts/deploy/send.sh
+++ local/scripts/deploy/send.sh
@@ -5,10 +5,12 @@ JENKINS_WORKSPACE=/var/jenkins_home/workspace/Frontend
 
 echo "Sending assets..."
 
+#sshpass -p $PROD_PASS scp $MAC_WORKSPACE/local/typescript-app/dist/frontend/deploy.zip  root@191.96.251.3:/var/www/snoweegamecorp.com/html/
 sshpass -p $PROD_PASS scp $JENKINS_WORKSPACE/local/typescript-app/dist/frontend/deploy.zip  root@191.96.251.3:/var/www/snoweegamecorp.com/html/
 
 echo "Sending scripts..."
 
+#sshpass -p $PROD_PASS scp $MAC_WORKSPACE/remote/scripts/deploy/extract_assets.sh  root@191.96.251.3:/root/
 sshpass -p $PROD_PASS scp $JENKINS_WORKSPACE/remote/scripts/deploy/extract_assets.sh  root@191.96.251.3:/root/
 
 echo "Sending scripts DONE!"
