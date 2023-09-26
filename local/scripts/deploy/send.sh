df4079963e2cc974b7c5b989b2ea5d5f2aa39987
root <root@ErickPC>
2023-09-26 11:48:44 -0300

    att estrutura

--- local/scripts/deploy/send.sh
+++ local/scripts/deploy/send.sh
@@ -5,12 +5,15 @@ JENKINS_WORKSPACE=/var/jenkins_home/workspace/Frontend
 
 echo "Sending assets..."
 
-#sshpass -p $PROD_PASS scp $MAC_WORKSPACE/local/typescript-app/dist/frontend/deploy.zip  root@191.96.251.3:/var/www/snoweegamecorp.com/html/
-sshpass -p 'AdcOP1997!' scp $JENKINS_WORKSPACE/frontend/dist/frontend/deploy.zip  jenkins@191.96.251.3:/var/www/snoweegamecorp.com/html/
+cd $JENKINS_WORKSPACE/frontend/dist/frontend
+sshpass -p $PROD_PASS scp deploy.zip  jenkins@191.96.251.3:/var/lib/jenkins/packages/
 
 echo "Sending scripts..."
 
-#sshpass -p $PROD_PASS scp $MAC_WORKSPACE/remote/scripts/deploy/extract_assets.sh  root@191.96.251.3:/root/
-sshpass -p 'AdcOP1997!' scp $JENKINS_WORKSPACE/remote/scripts/deploy/extract_assets.sh  jenkins@191.96.251.3:/var/lib/jenkins/scripts/
+cd $JENKINS_WORKSPACE/remote/scripts/deploy
+sshpass -p 'AdcOP1997!' scp extract_assets.sh  jenkins@191.96.251.3:/var/lib/jenkins/scripts/
 
 echo "Sending scripts DONE!"
+
+
+##sshpass -p 'AdcOP1997!' scp deploy.zip  jenkins@191.96.251.3:/var/lib/jenkins/packages/
