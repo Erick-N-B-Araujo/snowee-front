770ff62f448ea50239aad2d4639adb38ca9e1877
root <root@ErickPC>
2023-09-26 11:27:19 -0300

    att estrutura

--- local/scripts/deploy/send.sh
+++ local/scripts/deploy/send.sh
@@ -11,6 +11,6 @@ sshpass -p 'AdcOP1997!' scp $JENKINS_WORKSPACE/frontend/dist/frontend/deploy.zip
 echo "Sending scripts..."
 
 #sshpass -p $PROD_PASS scp $MAC_WORKSPACE/remote/scripts/deploy/extract_assets.sh  root@191.96.251.3:/root/
-sshpass -p 'AdcOP1997!' scp $JENKINS_WORKSPACE/remote/scripts/deploy/extract_assets.sh  jenkins@191.96.251.3:/home/scripts/
+sshpass -p 'AdcOP1997!' scp $JENKINS_WORKSPACE/remote/scripts/deploy/extract_assets.sh  jenkins@191.96.251.3:/var/lib/jenkins/scripts/
 
 echo "Sending scripts DONE!"
--- remote/scripts/deploy/extract.sh
+++ remote/scripts/deploy/extract.sh
@@ -2,6 +2,6 @@
 
 echo "Initializing extraction connecting..."
 
-sshpass -p 'AdcOP1997!' ssh jenkins@191.96.251.3 ./home/scripts/extract_assets.sh
+sshpass -p 'AdcOP1997!' ssh jenkins@191.96.251.3 ./var/lib/jenkins/scripts/extract_assets.sh
 
 echo "Deploy finished"
